1e099f1edcd4dd3a637a3217b6b8695174dbc0b4
Fanxi Zeng <fxzeng@yahoo-inc.com>
2014-12-15 17:30:42 +0800

    Initial git setup

---
+++ src/org/vicinity/index/IndexedStatement.java
@@ -0,0 +1,13 @@
+class IndexedStatement
+{
+    public List<LocatedStatement> retrieveStatements(Location location, Query query)
+    {
+    }
+
+    Map<LocationArea, > _locationToStatementIndicesMap;
+}
+
+class StateIndices
+{
+    protected Map<String, List<StatementId>> _wordToStatementIdsMap;
+}
